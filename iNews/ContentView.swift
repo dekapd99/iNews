667a0f1cdfa0678966b2d4a7294a2404fefe02e2
dekapd99 <dekaprimatio@gmail.com>
2022-06-01 20:56:48 +0700

    Initial Commit

---
+++ iNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iNews
+//
+//  Created by Deka Primatio on 01/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iNews/iNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNewsApp.swift
+//  iNews
+//
+//  Created by Deka Primatio on 01/06/22.
+//
+
+import SwiftUI
+
+@main
+struct iNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
