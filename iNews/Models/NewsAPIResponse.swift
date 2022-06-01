8f70026ad8e22f390f99c553f35392a94df7049a
dekapd99 <dekaprimatio@gmail.com>
2022-06-01 22:29:19 +0700

    Perbaikan di NewsAPIResponse

---
+++ iNews/Models/NewsAPIResponse.swift
@@ -0,0 +1,21 @@
+//
+//  NewsAPIResponse.swift
+//  iNews
+//
+//  Created by Deka Primatio on 01/06/22.
+//
+
+// Handling API Response Code
+import Foundation
+
+struct NewsAPIResponse: Decodable {
+    
+    let status: String
+    let totalResults: Int? // optional karena bisa return error
+    let articles: [Article]? // optional karena bisa tidak menampilkan apa apa kalo codingnya salah
+    
+    // Error handling untuk Code API Response News
+    let code: String?
+    let message: String?
+    
+}
--- iNews/Views/ArticleRowView.swift
+++ iNews/Views/ArticleRowView.swift
@@ -12,12 +12,36 @@ struct ArticleRowView: View {
     // menggunakan file Article dari folder Models
     let article: Article
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        // untuk memunculkan gambar ketika loading
+        VStack(alignment: .leading, spacing: 16){
+            AsyncImage(url: article.imageURL) { phase in
+                switch phase {
+                case .empty:
+                    ProgressView()
+                case . success(let image):
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                    
+                case .failure:
+                    Image(systemName: "photo")
+                    
+                @unknown default:
+                    fatalError()
+                }
+            }
+        }
     }
 }
 
 struct ArticleRowView_Previews: PreviewProvider {
     static var previews: some View {
-        ArticleRowView(article: .previewData[0])
+        List {
+            // tampilan ui ketika preview artikel
+            ArticleRowView(article: .previewData[0])
+                .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+        }
+        .listStyle(.plain) // style list content
     }
 }
