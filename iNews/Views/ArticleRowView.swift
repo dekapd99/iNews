816b2d4f4c18c52befc757a9194c618ea5c0a60e
dekapd99 <dekaprimatio@gmail.com>
2022-06-03 19:41:37 +0700

    merging and solve typo bug

--- iNews/Views/ArticleRowView.swift
+++ iNews/Views/ArticleRowView.swift
@@ -14,34 +14,52 @@ struct ArticleRowView: View {
     var body: some View {
         
         // untuk memunculkan gambar ketika loading
-        VStack(alignment: .leading, spacing: 16){
+        VStack(alignment: .leading, spacing: 16){ // Vertical Stack
             AsyncImage(url: article.imageURL) { phase in
                 switch phase {
-                case .empty:
-                    ProgressView()
-                case . success(let image):
+                case .empty: // Skeleton ketika gambar sedang loading
+                    HStack { // Horizontal Stack
+                        Spacer()
+                        ProgressView()
+                        Spacer()
+                    }
+                    
+                case .success(let image): // ketika berhasil loading
                     image
                         .resizable()
                         .aspectRatio(contentMode: .fill)
                     
-                case .failure:
-                    Image(systemName: "photo")
+                case .failure: // ketika error API Code Response
+                    HStack { // Horizontal Stack
+                        Spacer()
+                        Image(systemName: "photo").imageScale(.large)
+                        Spacer()
+                    }
                     
                 @unknown default:
                     fatalError()
                 }
             }
+            .frame(minHeight: 200, maxHeight: 300)
+            .background(Color.gray.opacity(0.3))
+            
+            VStack(alignment: .leading, spacing: 3) {
+                Text(article.title)
+            }
+            .padding([.horizontal, .bottom])
         }
     }
 }
 
 struct ArticleRowView_Previews: PreviewProvider {
     static var previews: some View {
-        List {
-            // tampilan ui ketika preview artikel
-            ArticleRowView(article: .previewData[0])
-                .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+        NavigationView {
+            List {
+                // tampilan ui ketika preview artikel
+                ArticleRowView(article: .previewData[0])
+                    .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+            }
+            .listStyle(.plain) // style list content
         }
-        .listStyle(.plain) // style list content
     }
 }
